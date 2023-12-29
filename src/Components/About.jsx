e7540261f509de14c42e7acbb3dfed71c88b3497
M. Abdullah Bin Aftab <110668234+heighterses@users.noreply.github.com>
2023-12-29 02:47:36 +0000

    changes in about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,28 +16,20 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/port.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = "App Marketer (ASO, SEO) | Aspiring Software Engineer ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["Web design", "User experience"];
 
 /**
  * Use this to give more information about what you are passionate about,
